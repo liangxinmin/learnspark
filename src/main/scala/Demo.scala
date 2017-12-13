214580fd51605b5203729380428abd5259f2c5a9
liangxinmin <xinminliang@creditease.cn>
2017-12-13 14:50:33 +0800

    Demo

---
+++ src/main/scala/Demo.scala
@@ -0,0 +1,13 @@
+object Demo {
+    implicit class IntTimes(x: Int) {
+        def times [A](f: => A): Unit = {
+            def loop(current: Int): Unit = {
+                if (current > 0) {
+                    f
+                    loop(current - 1)
+                }
+            }
+            loop(x)
+        }
+    }
+}
